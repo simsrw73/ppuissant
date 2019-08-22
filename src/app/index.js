658a7b037e55e1874b7320a5a04eff63a2141db3
Randy W. Sims <simsrw73@gmail.com>
2019-08-22 19:34:55 -0400

    Fix ref to uikit js libs

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,10 @@
 // @flow
 
-// import '~uikit/dist/js/uikit.js';
-// import "~uikit/dist/js/uikit-icons.js";
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
 
 
 document.getElementById('message').innerText = "Hello, World!";
