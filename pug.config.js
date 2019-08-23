060305d4491d9d3acf230d74a09f01ea0a104810
Randy W. Sims <simsrw73@gmail.com>
2019-08-23 12:11:51 -0400

    get name, desc, auth from package.json

---
+++ pug.config.js
@@ -0,0 +1,9 @@
+const packageJSON = require("./package.json");
+
+module.exports = {
+  locals: {
+    name: packageJSON.name,
+    desc: packageJSON.description,
+    author: packageJSON.author
+  }
+};
